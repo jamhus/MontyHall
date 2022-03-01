348cc5a3de9933c21aa3b04456b0784cf7b8cd2e
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-01 21:45:04 +0100

    added test to the Details component

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Details from "./components/Details";
+import { IGameRes } from "./interfaces/GameRes";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("If ParentComponent is passed data, ChildComponent is called with prop open and data", () => {
+  const data: IGameRes = {
+    tries: 100,
+    result: 50,
+    change: true,
+  };
+  render(<Details {...data} />);
+
+  const result = screen.getByTestId("result");
+  expect(result).toBeInTheDocument();
+  expect(result).toHaveTextContent("With 100 tries and changing is true , your chanses of winning is 50%");
 });
--- client/src/components/Details.tsx
+++ client/src/components/Details.tsx
@@ -5,7 +5,7 @@ import { IGameRes } from "../interfaces/GameRes";
 const Details = ({ tries, result, change }: IGameRes) => {
   const formatResult = () =>
     `With ${tries} tries and changing is ${change} , your chanses of winning is ${result}%`;
-  return <Typography variant="h3" gutterBottom sx={{ p: 2, justifyContent: 'center' , display: 'flex' }}>{formatResult()}</Typography>;
+  return <Typography variant="h3" gutterBottom sx={{ p: 2, justifyContent: 'center' , display: 'flex' }} data-testid="result">{formatResult()}</Typography>;
 };
 
 export default Details;
